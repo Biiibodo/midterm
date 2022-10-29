35fbd921d3a6c5c8a561588fa99418a43f7c3556
Biiibodo <avnish.work3@gmail.com>
2022-10-29 19:04:47 -0400

    Addedd routes for Booklists page

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //remote link to atlas deployment
+  //remote link to atlas 
   "URI": "mongodb+srv://kush:<password>@mongodbserver.nqzpzv5.mongodb.net/?retryWrites=true&w=majority"
 };
