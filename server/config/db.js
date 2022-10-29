e2f730b78aadd5cc808ca1978a92854d1772741a
Biiibodo <avnish.work3@gmail.com>
2022-10-29 19:20:04 -0400

    error fixing

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //remote link to atlas 
-  "URI": "mongodb+srv://kush:f#nni85R9.i4TU7@mongodbserver.nqzpzv5.mongodb.net/book_store?retryWrites=true&w=majority"
+  "URI": "mongodb://localhost/books229"
 };
